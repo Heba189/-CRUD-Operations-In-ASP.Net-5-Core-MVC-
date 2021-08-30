ede2c427082a8d742e0f7d1fbd5820ee06ee3eed
heba189 <Ultimate@DESKTOP-1QGBQMI>
2021-08-31 01:01:29 +0200

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [dbo].[Genres] ON
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(1,N'Action')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(2,N'Drama')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(3,N'Crime')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(4,N'Adventure')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(5,N'Romance')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(6,N'War')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(7,N'Fantasy')
+INSERT INTO [dbo].[Genres] ([Id],[Name]) VALUES(8,N'Historical')
